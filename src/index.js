033e5bdaac8727876bdf171ac8f61d2242396411
kanstantsin-laurenka <kanstantsin.laurenka@kufar.by>
2018-09-27 16:26:25 +0300

    Implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+  var trimmedString = equation.replace(/ /g, '');
+  var match = trimmedString.match(/(-?)\d+/g);
+  var a = Number(match[0]);
+  var b = Number(match[2]);
+  var c = Number(match[3]);
+
+  var d = Math.pow(b, 2) - 4 * a * c;
+
+  if (d < 0) return;
+
+  var x1 = ((-b + Math.sqrt(d)) / (2 * a)).toFixed(0);
+  var x2 = ((-b - Math.sqrt(d)) / (2 * a)).toFixed(0);
+
+  var result = [x1, x2];
+
+  return result.sort(function(a, b) {return a - b;});
+};
